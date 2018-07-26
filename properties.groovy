1ebbe0055a063c1673a670bc01142ece2809a6d9
wittyneko <wittytutu@gmail.com>
2018-07-26 16:46:55 +0800

    update configPath to https://gitee.com/wittyneko/gradle-tools/raw/master

--- properties.groovy
+++ properties.groovy
@@ -49,7 +49,7 @@ ext {
 
 ext {
     if (!rootProject.hasProperty('configPath')) {
-        def __configPath = "$rootDir"
+        def __configPath = "https://gitee.com/wittyneko/gradle-tools/raw/master"
         def _configPath = _ConfigProperties.getProperty('configPath', __configPath)
         configPath = _LocalProperties.getProperty('configPath', _configPath)
         if (configPath.startsWith('~')) {
